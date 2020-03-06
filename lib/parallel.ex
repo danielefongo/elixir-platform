e4a3b33d40cde7655587b6447ba393bf1f23de60
danielefongo <danielefongo@gmail.com>
2020-03-06 10:51:37 +0100

    first commit

---
+++ lib/parallel.ex
@@ -0,0 +1,18 @@
+defmodule Parallel do
+  @moduledoc """
+  Documentation for Parallel.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Parallel.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
