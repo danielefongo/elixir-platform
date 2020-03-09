20cbad8acae51388e5affba74b202726eb69065d
danielefongo <danielefongo@gmail.com>
2020-03-09 13:22:13 +0100

    add supervisor

--- example.exs
+++ example.exs
@@ -1,6 +1,9 @@
-Parallel.Cache.start_link(Parallel.Bucket)
+Parallel.Supervisor.start_link
 _ = Parallel.Cache.bucket("1")
 bucket = Parallel.Cache.bucket("1")
 Parallel.Bucket.put(bucket, "key", "value")
+
+Process.whereis(:cache) |> Process.exit(:kill)
+
 value = Parallel.Bucket.get(bucket, "key")
 IO.inspect value
